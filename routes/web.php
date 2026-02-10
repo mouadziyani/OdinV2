5650d3dbca3d8b5eef06fa74ce92ad52588dcc5b
MOEUAED <ziyanimouad@gmail.com>
2026-02-10 16:59:31 +0100

    Add LinkController with CRUD and auth middleware, update web routes for links

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +13,18 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+        //profile routing
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        //links routing
+    Route::get('/links', [LinkController::class, 'index'])->name('links.index');
+    Route::get('/links/create', [LinkController::class, 'create'])->name('links.create');
+    Route::post('/links', [LinkController::class, 'store'])->name('links.store');
+    Route::get('/links/{link}', [LinkController::class, 'show'])->name('links.show');
+    Route::get('/links/{link}/edit', [LinkController::class, 'edit'])->name('links.edit');
+    Route::put('/links/{link}', [LinkController::class, 'update'])->name('links.update');
+    Route::delete('/links/{link}', [LinkController::class, 'destroy'])->name('links.destroy');
 });
 
 require __DIR__.'/auth.php';
