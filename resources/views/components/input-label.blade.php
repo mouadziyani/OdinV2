750cc339eb4ef414b81284c3d67202c2a239e131
MOEUAED <ziyanimouad@gmail.com>
2026-03-11 10:06:31 +0000

    ui: update input label component

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,17 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+<label {{ $attributes->merge([
+    'class' => '
+        block 
+        text-[10px] 
+        font-black 
+        uppercase 
+        tracking-[0.15em] 
+        text-gray-500 
+        dark:text-gray-400 
+        mb-2 
+        ml-1
+    '
+]) }}>
     {{ $value ?? $slot }}
 </label>
