2065c285cd6e01459593cc784828c96f3ed51ac7
MOEUAED <ziyanimouad@gmail.com>
2026-03-11 10:06:31 +0000

    ui: update text input component

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,17 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge([
+    'class' => '
+        block w-full px-4 py-3 
+        bg-gray-50 dark:bg-gray-950 
+        border-gray-200 dark:border-gray-800 
+        text-gray-900 dark:text-gray-100 
+        placeholder-gray-400 dark:placeholder-gray-600
+        rounded-2xl shadow-sm 
+        transition-all duration-200
+        focus:bg-white dark:focus:bg-gray-900
+        focus:ring-4 focus:ring-blue-500/10 
+        focus:border-blue-500 dark:focus:border-blue-400
+        disabled:opacity-50 disabled:bg-gray-100 dark:disabled:bg-gray-800
+    '
+]) }}>
