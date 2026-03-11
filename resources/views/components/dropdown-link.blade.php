40d099a325e17cbee2783b36c68728b0f3f8d624
MOEUAED <ziyanimouad@gmail.com>
2026-03-11 10:06:31 +0000

    ui: update dropdown link component

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,16 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge([
+    'class' => '
+        block w-full px-5 py-3 
+        text-start text-[11px] font-black uppercase tracking-[0.1em] 
+        text-gray-600 dark:text-gray-400 
+        hover:bg-blue-50/50 dark:hover:bg-blue-900/20 
+        hover:text-blue-700 dark:hover:text-blue-400 
+        focus:outline-none focus:bg-blue-50 dark:focus:bg-blue-900/30 
+        transition-all duration-200 ease-in-out
+        first:rounded-t-xl last:rounded-b-xl
+    '
+]) }}>
+    <div class="flex items-center gap-3">
+        {{ $slot }}
+    </div>
+</a>
